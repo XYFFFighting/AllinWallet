37b58e2cb2fac1b408f1475fcc66c2a6d568f132
MizunareiXYF <765044145@qq.com>
2018-09-22 21:50:46 -0400

    let successful route to main page

---
+++ app/src/main/java/group26/cs307/allinwallet/MainPage.java
@@ -0,0 +1,14 @@
+package group26.cs307.allinwallet;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class MainPage extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+    }
+
+}
