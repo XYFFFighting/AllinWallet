f923a665bbcd390308f9418c2dd429219f55766d
Ray <chen2733@purdue.edu>
2018-09-29 16:53:43 -0400

    __NIR__ sth

--- app/src/main/java/group26/cs307/allinwallet/MainActivity.java
+++ app/src/main/java/group26/cs307/allinwallet/MainActivity.java
@@ -1,16 +1,27 @@
 package group26.cs307.allinwallet;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "DocSnippets";
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
     private FirebaseAnalytics mFirebaseAnalytics;
-    private Button signup, login, main;
+    private Button signup, login, main, datatest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         signup = (Button) findViewById(R.id.btn_signup);
         login = (Button) findViewById(R.id.btn_login);
         main = (Button) findViewById(R.id.btn_dashboard);
+        datatest = (Button) findViewById(R.id.data_test);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -40,8 +52,39 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        datatest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addAlanTuring();
+            }
+        });
+
     }
 
+    public void addAlanTuring() {
+        // Create a new user with a first, middle, and last name
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Alan");
+        user.put("middle", "Mathison");
+        user.put("last", "Turing");
+        user.put("born", 1912);
+
+// Add a new document with a generated ID
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+    }
 
 
 }
