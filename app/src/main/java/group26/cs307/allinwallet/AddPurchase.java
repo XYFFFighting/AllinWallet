aaddcbceb3e5a0e00c28fe7de574cf231d1fba5c
MizunareiXYF <765044145@qq.com>
2018-09-29 18:16:20 -0400

    2hrs send purchse data

--- app/src/main/java/group26/cs307/allinwallet/AddPurchase.java
+++ app/src/main/java/group26/cs307/allinwallet/AddPurchase.java
@@ -7,9 +7,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class AddPurchase extends AppCompatActivity {
     private Button add;
     private EditText inputName, inputPrice;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FirebaseAuth auth;
+    private FirebaseAnalytics mFirebaseAnalytics;
     private static final String TAG = "AllinWallet";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +36,20 @@ public class AddPurchase extends AppCompatActivity {
                 String price = inputPrice.getText().toString();
                 Log.d(TAG, "Item Name: " + name);
                 Log.d(TAG, "Item Price: " + price);
+
             }
         });
+
+    }
+
+    public void addPurchase(String name, String price) {
+        String uid = auth.getUid();
+        Map<String, Object> purchaselist = new HashMap<>();
+        purchaselist.put("uid", uid);
+        purchaselist.put("name", name);
+        purchaselist.put("price", price);
+        CollectionReference purchase = db.collection("purchase");
+        purchase.document(uid).set(purchaselist);
 
     }
 
