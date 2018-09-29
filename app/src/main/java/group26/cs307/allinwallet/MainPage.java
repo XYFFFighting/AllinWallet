5ac058081ae2b47f92c1a21bf6509d67929b97af
Ray <chen2733@purdue.edu>
2018-09-29 17:50:43 -0400

    30mins MainPage addPurchase button

--- app/src/main/java/group26/cs307/allinwallet/MainPage.java
+++ app/src/main/java/group26/cs307/allinwallet/MainPage.java
@@ -1,15 +1,28 @@
 package group26.cs307.allinwallet;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.app.Activity;
+
+import android.view.View;
+import android.widget.Button;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainPage extends AppCompatActivity {
+    private Button purchaseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+
+        purchaseButton = (Button) findViewById(R.id.addPurchase);
+
+        purchaseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainPage.this, AddPurchase.class));
+            }
+        });
     }
 
 }
