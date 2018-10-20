c4a62be5167ff871d8c34d28ff777f1061b8b4ad
Junyi Zhang <zhan2539@purdue.edu>
2018-10-20 18:21:03 -0400

    1hrs intial avatar

--- app/src/main/java/group26/cs307/allinwallet/Profile.java
+++ app/src/main/java/group26/cs307/allinwallet/Profile.java
@@ -11,6 +11,25 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+//jenny
+import java.io.File;
+import android.app.Activity;
+//import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+
+//jenny
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
