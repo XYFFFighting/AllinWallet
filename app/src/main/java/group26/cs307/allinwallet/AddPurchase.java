ea6c830aa194a4ed7d9bb8528f19559dc6867eda
MizunareiXYF <xu881@purdue.edu>
2018-10-27 16:57:34 -0400

    2hrs add location

--- app/src/main/java/group26/cs307/allinwallet/AddPurchase.java
+++ app/src/main/java/group26/cs307/allinwallet/AddPurchase.java
@@ -1,7 +1,18 @@
 package group26.cs307.allinwallet;
 
+import android.Manifest;
 import android.app.DatePickerDialog;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
@@ -11,11 +22,16 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import org.w3c.dom.Text;
+
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -27,9 +43,10 @@ import java.util.Locale;
 import java.util.Map;
 
 public class AddPurchase extends AppCompatActivity {
-    private Button save, cancel;
+    private Button save, cancel, getlocation;
     private Spinner categoryPicker;
     private EditText inputName, inputPrice, inputDate;
+    private TextView txt_location;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     private FirebaseAuth auth;
     private FirebaseAnalytics mFirebaseAnalytics;
@@ -40,7 +57,6 @@ public class AddPurchase extends AppCompatActivity {
     private SimpleDateFormat formatter;
     private int passedPurchaseIndex;
     private PurchaseItem item;
-
     public static List<String> defaultCategories = new ArrayList<>(Arrays.asList("Grocery",
             "Clothes", "Housing", "Personal", "General", "Transport", "Fun"));
     private List<String> categories;
@@ -58,10 +74,12 @@ public class AddPurchase extends AppCompatActivity {
 
         save = (Button) findViewById(R.id.save_button);
         cancel = (Button) findViewById(R.id.cancel_button);
+        getlocation = (Button) findViewById(R.id.btn_get_location);
         categoryPicker = (Spinner) findViewById(R.id.category_picker);
         inputName = (EditText) findViewById(R.id.item_name);
         inputPrice = (EditText) findViewById(R.id.item_price);
         inputDate = (EditText) findViewById(R.id.item_date);
+        txt_location = (TextView) findViewById(R.id.txt_location);
         categories = new ArrayList<>();
         categories.addAll(defaultCategories);
         // TO-DO: get categories from firebase
@@ -133,6 +151,27 @@ public class AddPurchase extends AppCompatActivity {
             }
         });
 
+        getlocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(new String[] {Manifest.permission.ACCESS_COARSE_LOCATION}, 1000);
+                } else {
+                    LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                    Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                    try {
+                        String city = getCityName(location.getLatitude(), location.getLongitude());
+                        txt_location.setText(city);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        Toast.makeText(AddPurchase.this, "Not found", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+
+
         if (passedPurchaseIndex != -1) {
             setTitle(R.string.title_activity_edit_purchase);
             inputName.setText(item.getTitle());
@@ -176,4 +215,51 @@ public class AddPurchase extends AppCompatActivity {
                 .collection("purchase").document(documentUID).update(purchaselist);
         Log.d(TAG, uid + " update purchase data");
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        switch (requestCode) {
+            case 1000:{
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                    Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                    try {
+                        String city = getCityName(location.getLatitude(), location.getLongitude());
+                        txt_location.setText(city);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        Toast.makeText(AddPurchase.this, "Not found", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(this, "Permission not granted!", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            }
+        }
+
+    }
+
+    private String getCityName(double latitude, double lontitude) {
+        Log.d(TAG, "latitude:" + latitude + " lontitude" + lontitude);
+        String cityName = "";
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        List<Address> addresses;
+        try {
+            addresses = geocoder.getFromLocation(latitude, lontitude, 10);
+            if (addresses.size() > 0) {
+                for(Address address : addresses) {
+                    if (address.getLocality() != null && address.getLocality().length() > 0) {
+                        cityName = address.getLocality();
+                        break;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return cityName;
+    }
 }
+
+
