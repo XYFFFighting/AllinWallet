548bd07b3336a01d769f6cf4cd4855dd6eb0a70b
Shaan Hoda <shaan.hoda@gmail.com>
2018-11-02 02:03:54 -0400

    -CategoriesActivity fully working now -You can now check the total amount for each category -Removed unnecessary code

--- app/src/main/java/group26/cs307/allinwallet/CategoriesActivity.java
+++ app/src/main/java/group26/cs307/allinwallet/CategoriesActivity.java
@@ -29,7 +29,7 @@ import java.util.Locale;
 
 public class CategoriesActivity extends AppCompatActivity {
     private FloatingActionButton purchaseButton;
-    private TextView welcomeMessage, budgetText;
+    private TextView welcomeMessage, groceryText, clothesText, housingText, personalText, generalText, transportText, funText;
     private RecyclerView purchaseList;
     private RecyclerView.Adapter purchaseListAdapter;
     private RecyclerView.LayoutManager purchaseListLayoutManager;
@@ -38,60 +38,26 @@ public class CategoriesActivity extends AppCompatActivity {
     private static final String TAG = "AllinWallet";
     private FirebaseAuth auth;
 
-    static double groceryAmount = 0;
-    static double clothesAmount = 0;
-    static double housingAmount = 0;
-    static double personalAmount = 0;
-    static double generalAmount = 0;
-    static double transportAmount = 0;
-    static double funAmount = 0;
-
-    /*switch (category) {
-        case "Grocery":
-            groceryAmount += price;
-            break;
-        case "Clothes":
-            clothesAmount += price;
-            break;
-        case "Housing":
-            housingAmount += price;
-            break;
-        case "Personal":
-            personalAmount += price;
-            break;
-        case "General":
-            generalAmount += price;
-            break;
-        case "Transport":
-            transportAmount += price;
-            break;
-        case "Fun":
-            funAmount += price;
-            break;
-    } */
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         auth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
 
-        welcomeMessage = (TextView) findViewById(R.id.welcomeText);
-        budgetText = (TextView) findViewById(R.id.budgetText);
-        setDate();
+        groceryText = (TextView) findViewById(R.id.GroceryText);
+        clothesText = (TextView) findViewById(R.id.ClothesText);
+        housingText = (TextView) findViewById(R.id.HousingText);
+        personalText = (TextView) findViewById(R.id.PersonalText);
+        generalText = (TextView) findViewById(R.id.GeneralText);
+        transportText = (TextView) findViewById(R.id.TransportText);
+        funText = (TextView) findViewById(R.id.FunText);
+
 
         //purchaseList = (RecyclerView) findViewById(R.id.purchase_list);
         //purchaseList.setHasFixedSize(true);
         //purchaseListLayoutManager = new LinearLayoutManager(CategoriesActivity.this);
         //purchaseList.setLayoutManager(purchaseListLayoutManager);
         purchases = new ArrayList<>();
-    }
-
-    public void setDate() {
-        Date today = Calendar.getInstance().getTime();//getting date
-        SimpleDateFormat formatter = new SimpleDateFormat("EEEE, MMMM d yyyy", Locale.getDefault());
-        String date = formatter.format(today);
-        welcomeMessage.append(date);
     }
 
     public void updateCategoryPage(String uid) {
@@ -102,21 +68,64 @@ public class CategoriesActivity extends AppCompatActivity {
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                 if (task.isSuccessful()) {
                     purchases.clear();
-                    budgetText.setText("Current spending: ");
-                    double sum = 0.0, amount;
+
+                    groceryText.setText("Groceries Total Amount: ");
+                    clothesText.setText("Clothes Total Amount: ");
+                    housingText.setText("Housing Total Amount: ");
+                    personalText.setText("Personal Total Amount: ");
+                    generalText.setText("General Total Amount: ");
+                    transportText.setText("Transport Total Amount: ");
+                    funText.setText("Fun Total Amount: ");
+
+
+                    double amount;
+                    double groceryAmount = 0.0;
+                    double clothesAmount = 0.0;
+                    double housingAmount = 0.0;
+                    double personalAmount = 0.0;
+                    double generalAmount = 0.0;
+                    double transportAmount = 0.0;
+                    double funAmount = 0.0;
 
                     for (QueryDocumentSnapshot document : task.getResult()) {
                         Log.d(TAG, document.getId() + "-->" + document.getData());
+                        String category = document.getString("category");
                         amount = document.getDouble("price");
-                        sum += amount;
-                        purchases.add(new PurchaseItem(
-                                document.getString("category"),
-                                document.getString("name"), amount,
-                                document.getDate("date"), document.getId()));
+                        assert category != null;
+                        switch (category) {
+                            case "Grocery":
+                                groceryAmount += amount;
+                                break;
+                            case "Clothes":
+                                clothesAmount += amount;
+                                break;
+                            case "Housing":
+                                housingAmount += amount;
+                                break;
+                            case "Personal":
+                                personalAmount += amount;
+                                break;
+                            case "General":
+                                generalAmount += amount;
+                                break;
+                            case "Transport":
+                                transportAmount += amount;
+                                break;
+                            case "Fun":
+                                funAmount += amount;
+                                break;
+                        }
                     }
 
                     //purchaseListAdapter.notifyDataSetChanged();
-                    budgetText.append(Double.toString(sum));
+                    groceryText.append(Double.toString(groceryAmount));
+                    clothesText.append(Double.toString(clothesAmount));
+                    housingText.append(Double.toString(housingAmount));
+                    personalText.append(Double.toString(personalAmount));
+                    generalText.append(Double.toString(generalAmount));
+                    transportText.append(Double.toString(transportAmount));
+                    funText.append(Double.toString(funAmount));
+
 
                     dRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
                         @Override
@@ -127,7 +136,7 @@ public class CategoriesActivity extends AppCompatActivity {
                                     Log.d(TAG, document.getId() + "-->" + document.getData());
 
                                     if (document.contains("budget")) {
-                                        budgetText.append(" / " + document.getDouble("budget"));
+                                        //budgetText.append(" / " + document.getDouble("budget"));
                                     }
                                 } else {
                                     Log.d(TAG, "No such document");
