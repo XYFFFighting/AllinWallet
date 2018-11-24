72a71ac8c971034b5f1cac70fedc2b082a77e9e0
zhan2539 <zhan2539@purdue.edu>
2018-11-24 14:59:59 -0500

    conflict

--- app/src/main/java/group26/cs307/allinwallet/GraphVisual.java
+++ app/src/main/java/group26/cs307/allinwallet/GraphVisual.java
@@ -1,6 +1,7 @@
 package group26.cs307.allinwallet;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
@@ -12,6 +13,12 @@ import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.github.mikephil.charting.utils.ColorTemplate;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -19,16 +26,17 @@ import java.util.Random;
 
 public class GraphVisual extends AppCompatActivity {
     BarChart barChart;
-
+    private FirebaseAuth auth;
+    private static final String TAG = "AllinWallet";
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
     ArrayList<String> theDates = new ArrayList<>();;
     Random random;
-
+    String[] monthss = new String[] {"Jan", "Feb", "Mar", "April", "May", "Jun","July","Aug","Sep","Oct","Nov","Dec"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.d("myTag", "nizaiganma!");
+        auth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
-        Log.d("myTag", "nizaiganma!");
         setContentView(R.layout.activity_graph_visual);
 
 
@@ -41,6 +49,34 @@ public class GraphVisual extends AppCompatActivity {
         barChart.setMaxVisibleValueCount(50);
         barChart.setPinchZoom(false);
         barChart.setDrawGridBackground(true);
+
+
+
+
+        String uid = auth.getUid();
+        final DocumentReference dRef = db.collection("users").document(uid);
+        dRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Log.d(TAG, document.getId() + "-->" + document.getData());
+
+
+                        if (document.contains("monthly budget")){
+
+                        }
+
+
+                    } else {
+                        Log.d(TAG, "No such document");
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
 
 
         ArrayList<BarEntry> barEntries = new ArrayList<>();
