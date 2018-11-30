9e8a26b8092b00a84d8386425f6b24a0e5b6efc7
rohithsri <rsridhar@purdue.edu>
2018-11-29 20:01:11 -0500

    BugFixes for themes, added black and white

---
+++ app/src/main/java/group26/cs307/allinwallet/CustomThemesActivity.java
@@ -0,0 +1,27 @@
+package group26.cs307.allinwallet;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class CustomThemesActivity extends AppCompatActivity {
+    private Button sendFeedback;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cus_themes);
+        //sendFeedback = (Button) findViewById(R.id.feedBackbtn);
+
+
+        /*sendFeedback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Feedback submitted", Toast.LENGTH_LONG).show();
+                //setContentView(R.layout.activity_main);
+            }
+        });*/
+    }
+
+}
